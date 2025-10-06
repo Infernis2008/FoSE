3e5ccc581e9933686e5c4f2a8032e54328f9781e
Савенков Павел <Savenkov_PV_25@student.grsu.by>
2025-10-06 19:37:20 +0300

    Delete pract 2 directory

--- pract 2/a6/main.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-total_sec = N % (24 * 3600)
-hours = total_sec // 3600
-minutes = (total_sec % 3600) // 60
-seconds = total_sec % 60
-print(f"{hours}:{minutes:02d}:{seconds:02d}")
--- pract 2/а5/main.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Введите число А: "))
-b=int(input("Введите число В: "))
-if a%b == 0:
-    print("YES")
-else:
-    print("NO")
